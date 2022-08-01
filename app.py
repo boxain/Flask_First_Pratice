a143457c258dd70150ead12f30579c474cf0902a
Aaron <aaronprogram0919@gmail.com>
2022-08-01 13:31:33 +0800

    second commit

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return 'Hellow'
+
+@app.route('/test')
+def test():
+    return 'This is test'
 
 if __name__=='__main__':
     app.run()
